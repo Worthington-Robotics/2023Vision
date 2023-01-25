b77fcb8fc9349a9ddf8ea383e1bb9c6c4cf3c67a
Chervix <wtooyah278@gmail.com>
2023-01-24 21:18:32 -0500

    added temperary testing features

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pupil_apriltags import Detector
 from vision.zed_init import zed_init
 from vision.zed_params import *
 from vision.vision_processing import VisionProcesser
+from xavier_testing import xavier_testing
 
 
 def main():
@@ -26,11 +27,17 @@ def main():
 
     vision_processer = VisionProcesser(detect, zed)
 
+    #delete before competition:
+    test = xavier_testing()
+    
     while True:
         if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
             tracking_state = zed.get_position(camera_pose)
             if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:
                 tx, ty, tz = vision_processer.track_position(camera_pose, zed)
+                #test.print_robot_pose(tx, ty, tz)
+                tag_pose = vision_processer.april_tag_tracking()
+                test.print_tag_pose(tag_pose)
         
 
 
---
+++ xavier_testing.py
@@ -0,0 +1,12 @@
+#delete before competition. This is for testing the code on the Xavier
+
+from operator import mod
+
+
+class xavier_testing:
+    def print_robot_pose(tx, ty, tz):
+        print(f"tx: {tx}")
+        print(f"tx: {ty}")
+        print(f"tx: {tz}")
+    def print_tag_pose(tag_pose):
+        print(f"tag pose: {tag_pose}")
