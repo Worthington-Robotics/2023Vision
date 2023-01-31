81aa7b8acf5287cc238fdfeff3c65465154abcdf
Chervix <wtooyah278@gmail.com>
2023-01-31 17:46:18 -0500

    Calculated how many times per second the robot pose was calculated that

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import numpy
 from pupil_apriltags import Detector
 from vision import VisionProcesser, zed_init, init_calibration_params, Dispatcher
 
+import time
+
 
 
 def main():
@@ -28,16 +30,20 @@ def main():
 
     vision_processer = VisionProcesser(detect, zed)
 
-    
-    while True:
+    start_time = time.time()
+    frame = 0
+
+    while time.time() - start_time < 60:
         if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS:
             tracking_state = zed.get_position(camera_pose)
             if tracking_state == sl.POSITIONAL_TRACKING_STATE.OK:
                 tx, ty, tz = vision_processer.track_position(camera_pose, zed)
+                frame = frame + 1
                 tag_pose = vision_processer.april_tag_tracking()
                 dispatcher.dispatch_robot_pose(tx, ty, tz)
                 dispatcher.dispatch_tag_pose(tag_pose)
-                
+    
+    print(frame/60)             
         
 
 
