83b57befbbd2e57f4c350b73e252ce9f03e7fa52
Chervix <122894453+SilentSchnitzel@users.noreply.github.com>
2023-03-13 23:45:58 -0400

    started drafting some of the object pose estimation code

--- experimental/object_pose.py
+++ experimental/object_pose.py
@@ -4,3 +4,22 @@
 
 #1 set up stereo camera system
 #2?
+
+import cv2
+import numpy as np
+import pyzed.sl as sl
+
+class ObjectPose:
+    def ObjectPose(self):
+        zed = sl.Camera()
+        init_params = sl.InitParams()
+        init_params.camera_resoultion = sl.RESOLUTION.HD720
+        init_params.camera_fps = 30
+        init_params.depth_mode = sl.DEPTH_MODE.ULTRA #what does this line of code do?
+        err = zed.open(init_params)
+        if err != sl.ERROR_CODE.SUCCESS:
+            print("error occured")
+            exit()
+
+
+    
